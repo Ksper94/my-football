9fa7ad554b947c206b9b15f870bb37577465b097
ksper94 <hugostephan94470@gmail.com>
2024-12-15 11:02:10 +0100

    Ajout de la configuration bodyParser désactivée pour le webhook Stripe et création de check-subscription API

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,11 +1,14 @@
 // utils/supabaseClient.js
 
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// Initialisez le client Supabase public (côté client)
-const supabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+// Récupérer les variables d'environnement
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY
 
-export { supabaseClient };
+// Client Supabase pour le frontend
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+// Client Supabase Admin pour le backend
+export const supabaseAdmin = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
