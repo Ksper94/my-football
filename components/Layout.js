b42b5c9baf0c2139ae1364094ca47822196d0cdb
ksper94 <hugostephan94470@gmail.com>
2024-12-31 16:19:11 +0100

    Message décrivant vos modifications

--- components/Layout.js
+++ components/Layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children }) {
         {/* Ajout du lien vers l'index */}
         <Link href="/">
           <h1 className="text-2xl font-bold text-white cursor-pointer">
-            Foot predictions
+            Foot Predictions
           </h1>
         </Link>
         <nav>
